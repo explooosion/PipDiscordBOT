715700408b0bb2485b65aaf833a458476a72a69f
Robby <ta7382@gmail.com>
2018-02-20 02:14:30 +0800

    Update cmd.js add sky commands

--- src/lib/cmd.js
+++ src/lib/cmd.js
@@ -1,6 +1,9 @@
 import fs from 'fs';
+import fetch from 'node-fetch';
 
-module.exports.send = (bot, param) => {
+const wt = require('../api/weather/all.json');
+
+module.exports.send = async (bot, param) => {
 
     const {
         user,
@@ -16,6 +19,44 @@ module.exports.send = (bot, param) => {
     args = args.splice(1);
 
     switch (cmd) {
+        case 'help':
+
+            console.log(param);
+
+            bot.sendMessage({
+                to: channelID,
+                embed: {
+                    color: 0xff00ff,
+                    author: {
+                        name: 'Pip-Bot',
+                        icon_url: 'https://i.imgur.com/RZP1rAx.png',
+                    },
+                    title: '關於 [PipDiscordBOT](https://github.com/explooosion/PipDiscordBOT) 的資訊',
+                    description: 'Hi~ 以下為小妹的基本指令，不可以做色色的事情哦！',
+                    fields: [{
+                        name: 'Prefix',
+                        value: '`!` example: `!help',
+                    }, {
+                        name: 'Info Commands',
+                        value: '`ping` `help` `hi`',
+                    }, {
+                        name: 'Game Commands',
+                        value: '`pubg`',
+                    }, {
+                        name: 'Utility Commands',
+                        value: '`map [place]` `sky [town]`',
+                    }, {
+                        name: 'Unknow Commands',
+                        value: '`#0`',
+                    }],
+                    timestamp: new Date(),
+                    footer: {
+                        text: 'Pip-Bot Team'
+                    }
+                }
+            });
+            break;
+
         case 'ping':
             bot.sendMessage({
                 to: channelID,
@@ -67,45 +108,47 @@ module.exports.send = (bot, param) => {
             });
             break;
 
-        case 'help':
+        case 'sky':
 
-            console.log(param);
-
-            bot.sendMessage({
-                to: channelID,
-                embed: {
-                    color: 0xff00ff,
-                    author: {
-                        name: 'Pip-Bot',
-                        icon_url: 'https://i.imgur.com/RZP1rAx.png',
-                    },
-                    title: '關於 [PipDiscordBOT](https://github.com/explooosion/PipDiscordBOT) 的資訊',
-                    description: 'Hi~ 以下為小妹的基本指令，不可以做色色的事情哦！',
-                    fields: [{
-                        name: 'Prefix',
-                        value: '`!` example: `!help',
-                    }, {
-                        name: 'Info Commands',
-                        value: '`ping` `help` `hi`',
-                    }, {
-                        name: 'Game Commands',
-                        value: '`pubg`',
-                    }, {
-                        name: 'Utility Commands',
-                        value: '`map`',
-                    }, {
-                        name: 'Unknow Commands',
-                        value: '`#0`',
-                    }],
-                    timestamp: new Date(),
-                    footer: {
-                        text: 'Pip-Bot Team'
-                    }
+            wt.forEach(city => {
+                const town = city.towns.find(c => {
+                    return c.name.includes(args)
+                });
+                if (town) {
+                    console.log(town);
+                    const url = `https://works.ioa.tw/weather/api/weathers/${town.id}.json`;
+                    fetch(url)
+                        .then(res => res.json())
+                        .then(json => {
+                            bot.sendMessage({
+                                to: channelID,
+                                embed: {
+                                    color: 0xf1684e,
+                                    description: `${town.name}`,
+                                    image: {
+                                        url: `https://works.ioa.tw/weather/img/towns/${city.id}/${town.id}/v.jpg`,
+                                        // url: `https://works.ioa.tw/weather/img/weathers/zeusdesign/${town.img}`,
+                                    },
+                                    fields: [{
+                                        name: '天氣狀況',
+                                        value: `${json.desc}`,
+                                    }, {
+                                        name: '平均溫度',
+                                        value: `${json.temperature}`,
+                                    }],
+                                    timestamp: `${json.at}`,
+                                    footer: {
+                                        text: '©2014 - 2017 WWW.IOA.TW'
+                                    }
+                                }
+                            });
+                        });
                 }
             });
             break;
 
         default:
+
             break;
     }
 }
